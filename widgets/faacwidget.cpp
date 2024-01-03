88b4bc2714b6631f790b5e19b8c0962c4b380cd4
Marco Nelles <marco@maniatek.de>
2024-01-03 10:07:37 +0100

    Add embed cover function for FAAC and Opus

--- widgets/faacwidget.cpp
+++ widgets/faacwidget.cpp
@@ -22,6 +22,7 @@ faacWidget::faacWidget(Parameters *parameters, QWidget *parent)
 
     horizontalSlider_quality->setValue(parameters->value(ENCODER_FAAC_QUALITY_KEY, ENCODER_FAAC_QUALITY).toInt());
     kintspinbox_quality->setValue(parameters->value(ENCODER_FAAC_QUALITY_KEY, ENCODER_FAAC_QUALITY).toInt());
+    checkBox_embedcover->setChecked(parameters->value(ENCODER_FAAC_EMBED_COVER_KEY).toBool());
     qlineedit_suffix->setText(parameters->value(ENCODER_FAAC_SUFFIX_KEY, ENCODER_FAAC_SUFFIX).toString());
 
     connect(horizontalSlider_quality, SIGNAL(valueChanged(int)), this, SLOT(quality_changed_by_slider(int)));
@@ -29,6 +30,8 @@ faacWidget::faacWidget(Parameters *parameters, QWidget *parent)
 
     connect(kintspinbox_quality, SIGNAL(valueChanged(int)), this, SLOT(quality_changed_by_spinbox(int)));
     connect(kintspinbox_quality, SIGNAL(valueChanged(int)), this, SLOT(trigger_changed()));
+
+    connect(checkBox_embedcover, SIGNAL(toggled(bool)), this, SLOT(trigger_changed()));
 
     connect(qlineedit_suffix, SIGNAL(textEdited(const QString &)), this, SLOT(trigger_changed()));
 
@@ -44,6 +47,7 @@ bool faacWidget::save()
     bool success = true;
 
     parameters->setValue(ENCODER_FAAC_QUALITY_KEY, horizontalSlider_quality->value());
+    parameters->setValue(ENCODER_FAAC_EMBED_COVER_KEY, checkBox_embedcover->isChecked());
     parameters->setValue(ENCODER_FAAC_SUFFIX_KEY, qlineedit_suffix->text());
 
     changed = false;
@@ -68,6 +72,7 @@ void faacWidget::quality_changed_by_spinbox(int quality)
 void faacWidget::trigger_changed()
 {
     changed = (horizontalSlider_quality->value() != parameters->value(ENCODER_FAAC_QUALITY_KEY, ENCODER_FAAC_QUALITY).toInt()
+               || checkBox_embedcover->isChecked() != parameters->value(ENCODER_FAAC_EMBED_COVER_KEY).toBool()
                || qlineedit_suffix->text() != parameters->value(ENCODER_FAAC_SUFFIX_KEY, ENCODER_FAAC_SUFFIX).toString());
 
     Q_EMIT triggerChanged();
