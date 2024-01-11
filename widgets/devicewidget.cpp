998f7abe39e9b3fd28081c4038f3a564b980af73
Marco Nelles <marco@maniatek.de>
2024-01-11 17:06:15 +0100

    Show device information in settings dialog

--- widgets/devicewidget.cpp
+++ widgets/devicewidget.cpp
@@ -16,3 +16,23 @@ deviceWidget::deviceWidget(QWidget *parent)
 deviceWidget::~deviceWidget()
 {
 }
+
+void deviceWidget::setDeviceInfo(const QString &vendor, const QString &model, const QString &revision)
+{
+    label_Vendor->setText(vendor);
+    label_Model->setText(model);
+    label_Revision->setText(revision);
+    label_Vendor->setEnabled(true);
+    label_Model->setEnabled(true);
+    label_Revision->setEnabled(true);
+}
+
+void deviceWidget::clearDeviceInfo()
+{
+    label_Vendor->setText(i18n("(No disc detected)"));
+    label_Model->setText(i18n("(No disc detected)"));
+    label_Revision->setText(i18n("(No disc detected)"));
+    label_Vendor->setEnabled(false);
+    label_Model->setEnabled(false);
+    label_Revision->setEnabled(false);
+}
